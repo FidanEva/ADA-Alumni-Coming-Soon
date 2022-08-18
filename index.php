bd1af8bd8090332922bba793e1ab7596b9d208ee
FidanEva <86987094+FidanEva@users.noreply.github.com>
2022-08-18 17:24:01 +0400

    ADA Alumni Association - Coming Soon

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title> ADA Alumni Association</title>
+    <!-- ========== Meta Tags ========== -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="ADA Alumni Association">
+    <meta property="og:image" content="/assets/img/ada-logo.jpeg" />
+
+    <!--===============================================================================================-->
+    <link rel="shortcut icon" href="images/icons/ada-logo.png" type="image/x-icon">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Bodoni&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Bodoni&family=My+Soul&family=Parisienne&display=swap"
+        rel="stylesheet">
+
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css?<?php echo time() ?>">
+    <link rel="stylesheet" type="text/css" href="css/main.css?<?php echo time() ?>">
+    <!--===============================================================================================-->
+</head>
+
+<body>
+    <div class="simpleslide100">
+        <div class="simpleslide100-item bg-img1" id="banner-video">
+            <video src="vid/ada.mp4" autoplay muted loop></video>
+        </div>
+    </div>
+
+    <div class="flex-col-c-sb size1 overlay1" style="display:flex; justify-content: space-between;">
+        <!--  -->
+        <div class="w-full flex-w flex-sb-m p-t-22 p-b-10">
+            <div class="wrappic1 d-flex justify-content-center m-t-10 m-b-10">
+                <a href="#"><img src="images/icons/ada-logo-w.png" alt="LOGO"></a>
+            </div>
+        </div>
+
+        <div class="flex-col-c-m p-t-10 p-b-10">
+            <h3 class="l1-txt1 txt-center p-b-35 respon1">
+                <strong class="title">
+                    ADA University
+                    <br>
+                    Alumni Association
+                </strong>
+                <br>
+                Coming Soon
+            </h3>
+        </div>
+
+        <div class="flex-col-c-m p-t-10 p-b-20">
+            <!-- HTML !-->
+            <a href="https://test.adaalumni.az/register.php" target="_blank">
+                <button class="button-63" role="button">Become a member</button>
+            </a>
+        </div>
+        <div class="flex-w flex-c-m p-t-10 p-b-10">
+            <a href="https://www.facebook.com/ADAUniversity" target="_blank"
+                class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+                <i class="fa fa-facebook"></i>
+            </a>
+            <a href="https://www.instagram.com/ada.university/" target="_blank"
+                class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+                <i class="fa fa-instagram"></i>
+            </a>
+            <a href="https://www.linkedin.com/company/adauaa/" target="_blank"
+                class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+                <i class="fa fa-linkedin"></i>
+            </a>
+            <a href="https://t.me/+eFxhSn8Hww9lMjdi" target="_blank"
+                class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+                <i class="fa fa-telegram"></i>
+            </a>
+        </div>
+
+        <div class="w-full flex-w flex-sb-m p-b-22 p-t-30">
+            <div class="flex-w m-t-10 m-b-10">
+                <a>
+                    <img src="https://www.butagrup.com.tr/images/header/logo.webp" alt="Buta Grup Logo"
+                        style="max-width: 200px; filter: brightness(0) invert(1);">
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <!--===============================================================================================-->
+    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/bootstrap/js/popper.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+
+    <!--===============================================================================================-->
+    <script src="vendor/tilt/tilt.jquery.min.js"></script>
+    <script>
+        $('.js-tilt').tilt({
+            scale: 1.1
+        })
+    </script>
+    <!--===============================================================================================-->
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
